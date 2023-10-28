a745dd0e21b56f8820ee35bf56181dc32c837ef4
frankpyf <frankpyf0620@outlook.com>
2023-10-28 19:29:15 +0800

    Init

---
+++ src/VulkanCore.h
@@ -0,0 +1,23 @@
+#include <vulkan/vulkan.h>
+
+namespace vme {
+    class VulkanCore
+    {
+    private:
+        VkInstance instance_;
+        /* data */
+    public:
+        VulkanCore(/* args */);
+        ~VulkanCore();
+    };
+    
+    VulkanCore::VulkanCore(/* args */)
+    {
+    }
+    
+    VulkanCore::~VulkanCore()
+    {
+    }
+    
+
+}
