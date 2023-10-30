68a6a2e310d33fbeeec5d70c72c9796f0dd87b7a
frankpyf <frankpyf0620@outlook.com>
2023-10-30 14:29:00 +0800

    Init

--- include/VulkanCore.h
+++ include/VulkanCore.h
@@ -3,9 +3,19 @@
 namespace vme {
     class VulkanCore
     {
+    public:
+        class Builder
+        {
+        public:
+            Builder& SetAppName(const char* name);
+        };
     private:
-        VkInstance instance_;
-        /* data */
+        VkInstance instance_; // Vulkan library handle
+	    VkDebugUtilsMessengerEXT debug_messenger_; // Vulkan debug output handle
+	    VkPhysicalDevice chosen_gpu_; // GPU chosen as the default device
+	    VkDevice device_; // Vulkan device for commands
+	    VkSurfaceKHR surface_; // Vulkan window surface
+
     public:
         VulkanCore(/* args */);
         ~VulkanCore();
